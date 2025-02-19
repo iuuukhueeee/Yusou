c199dff34e893957d346b9b45581f2cd5fc1f15f
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-02-19 04:49:44 +0000

    chore: add custom getObject s3

--- src/utils/s3/index.ts
+++ src/utils/s3/index.ts
@@ -1,4 +1,4 @@
-import { S3Client } from "@aws-sdk/client-s3"; // ES Modules import
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3" // ES Modules import
 
 export const s3Client = new S3Client({
   credentials: {
@@ -6,4 +6,25 @@ export const s3Client = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
   region: process.env.AWS_REGION!,
-});
+})
+
+export const getObject = (bucket: string, key: string) => {
+  return new Promise(async (resolve, reject) => {
+    const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: key })
+
+    try {
+      const response = await s3Client.send(getObjectCommand)
+      const responseDataChunks = []
+
+      if (response.Body) {
+        response.Body.once("error", (err) => reject(err))
+
+        response.Body.on("data", (chunk) => responseDataChunks.push(chunk))
+
+        response.Body.once("end", () => resolve(responseDataChunks.join("")))
+      }
+    } catch (error) {
+      return reject(error)
+    }
+  })
+}
