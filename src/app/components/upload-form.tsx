16cb24ee63cf100561b9401bae9d7546b58b9549
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-02-27 04:48:42 +0000

    feat: add upload files

--- src/app/components/upload-form.tsx
+++ src/app/components/upload-form.tsx
@@ -1,58 +1,80 @@
-"use client"
+'use client'
 
-import { Button, Flex, Textarea } from "@mantine/core"
-import { useDisclosure } from "@mantine/hooks"
+import { Button, Flex, Textarea } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { notifications } from '@mantine/notifications'
 
-import { useForm } from "@mantine/form"
-import { notifications } from "@mantine/notifications"
+import { useForm } from '@mantine/form'
 
-import CodeModal from "@/app/components/code-modal"
-import Dropbox from "@/app/components/dropbox"
-import { useMutation, useQuery } from "@tanstack/react-query"
-import { useState } from "react"
+import CodeModal from '@/app/components/code-modal'
+import Dropbox from '@/app/components/dropbox'
+import { FileWithPath } from '@mantine/dropzone'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { FormEvent, useState } from 'react'
 
 function UploadForm() {
   const [opened, { open, close }] = useDisclosure(false)
-  const [code, setCode] = useState<string>("")
+  const [code, setCode] = useState<string>('')
+  const [files, setFiles] = useState<FileWithPath[]>([])
 
   const form = useForm({
-    mode: "uncontrolled",
+    mode: 'uncontrolled',
     initialValues: {
-      text: "",
+      text: '',
     },
   })
 
   const objectMutation = useMutation({
-    mutationFn: async (values: { text: string; objectKey: string }) => {
-      return fetch("/api/s3", {
-        method: "PUT",
-        body: JSON.stringify(values),
+    mutationFn: async (values: { data: File | string; objectKey: string }) => {
+      const formData = new FormData()
+      formData.append('objectKey', values.objectKey)
+      formData.append('data', values.data)
+      return fetch('/api/s3', {
+        method: 'PUT',
+        body: formData,
       })
     },
   })
 
   const dbMutation = useMutation({
     mutationFn: async (values: { objectKey: string }) => {
-      return fetch("/api/share", {
-        method: "POST",
+      return fetch('/api/share', {
+        method: 'POST',
         body: JSON.stringify(values),
       })
     },
   })
 
   const { refetch: getUUID } = useQuery({
-    queryKey: ["/uuid/get"],
+    queryKey: ['/uuid/get'],
     queryFn: async () => {
-      const response = await fetch("/api/uuid", { method: "GET" })
+      const response = await fetch('/api/uuid', { method: 'GET' })
       return response.json()
     },
     enabled: false, // Disable automatic fetching
   })
 
-  const handleSubmit = async (values: { text: string }) => {
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    // generate a new UUID via API
     const { data: uuid } = await getUUID()
 
-    await objectMutation.mutateAsync({ text: values.text, objectKey: uuid.data })
+    const text = form.getValues().text
+
+    files.forEach(async (file) => {
+      await objectMutation.mutateAsync({
+        data: file,
+        objectKey: uuid.data,
+      })
+    })
+
+    if (text.length > 0) {
+      await objectMutation.mutateAsync({
+        data: text,
+        objectKey: uuid.data,
+      })
+    }
+
     const response = await dbMutation.mutateAsync({ objectKey: uuid.data })
     const { data: shares } = await response.json()
 
@@ -61,27 +83,27 @@ function UploadForm() {
       open()
     }
 
-    form.setFieldValue("text", "")
+    form.setFieldValue('text', '')
 
     notifications.show({
-      message: "Upload completed!",
+      message: 'Upload completed!',
     })
   }
 
   return (
     <>
       <CodeModal close={close} code={code} opened={opened} />
-      <form onSubmit={form.onSubmit(async (values) => await handleSubmit(values))}>
+      <form>
         <Flex gap="md" justify="center" align="center" direction="column" wrap="wrap" mt="lg">
           <Textarea
             label="Your message"
             className="w-9/12"
             placeholder="text..."
-            key={form.key("text")}
-            {...form.getInputProps("text")}
+            key={form.key('text')}
+            {...form.getInputProps('text')}
           />
-          <Dropbox />
-          <Button className="m-auto w-9/12" type="submit">
+          <Dropbox files={files} setFiles={setFiles} />
+          <Button className="m-auto w-9/12" type="submit" onClick={handleSubmit}>
             Go 🚚
           </Button>
         </Flex>
