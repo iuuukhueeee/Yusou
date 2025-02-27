79036f80cdb0cb6347875f968118a35132f0e2ab
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-02-27 05:15:33 +0000

    chore: add listObjects route handler

--- src/utils/s3/index.ts
+++ src/utils/s3/index.ts
@@ -1,11 +1,11 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3" // ES Modules import
+import { GetObjectCommand, ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3' // ES Modules import
 
 export const s3Client = new S3Client({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
   },
-  region: process.env.AWS_REGION ?? "",
+  region: process.env.AWS_REGION ?? '',
 })
 
 export const getObject = (bucket: string, key: string) => {
@@ -20,6 +20,28 @@ export const getObject = (bucket: string, key: string) => {
         const bodyAsString = await bodyStream.transformToString()
         resolve(bodyAsString)
       }
+    } catch (error) {
+      return reject(error)
+    }
+  })
+}
+
+export const listObjects = (bucket: string, key: string) => {
+  return new Promise<string[]>(async (resolve, reject) => {
+    const listObjectsCommand = new ListObjectsV2Command({ Bucket: bucket, Prefix: key })
+
+    try {
+      const response = await s3Client.send(listObjectsCommand)
+      console.log(response)
+      const names: string[] = []
+
+      if (response.KeyCount && response.KeyCount > 0) {
+        response.Contents?.forEach((obj) => {
+          if (obj && obj.Key) names.push(obj.Key)
+        })
+      }
+
+      resolve(names)
     } catch (error) {
       return reject(error)
     }
