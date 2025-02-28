476d4f9e8fc8a687864b452fdd5ffb0c28d7f15e
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-02-28 05:46:48 +0000

    feat: generate presigned url

--- src/utils/s3/index.ts
+++ src/utils/s3/index.ts
@@ -1,4 +1,5 @@
 import { GetObjectCommand, ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3' // ES Modules import
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 export const s3Client = new S3Client({
   credentials: {
@@ -41,9 +42,23 @@ export const listObjects = (bucket: string, key: string) => {
         })
       }
 
+      for (let i = 0; i < names.length; i++) {
+        const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: names[i] })
+        names[i] = await generatePresignedUrl(s3Client, getObjectCommand)
+      }
+
       resolve(names)
     } catch (error) {
       return reject(error)
     }
   })
+}
+
+async function generatePresignedUrl(client: S3Client, command: GetObjectCommand): Promise<string> {
+  try {
+    const url = await getSignedUrl(client, command, { expiresIn: 3600 })
+    return url
+  } catch (error) {
+    throw error
+  }
 }
