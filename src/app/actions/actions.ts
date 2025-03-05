76c4502f15f106dfc3e4560408e577ea2ea4fc22
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-03-05 03:57:19 +0000

    chore: add verifySite recaptcha challenge

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -2,6 +2,7 @@
 
 import { listObjects } from '@/utils/s3'
 import { createClient } from '@/utils/supabase/server'
+import { headers } from 'next/headers'
 
 export async function getData(code: string) {
   try {
@@ -22,6 +23,32 @@ export async function getData(code: string) {
     }
 
     throw Error('Missing object key')
+  } catch (error) {
+    throw error
+  }
+}
+
+export async function verifySite(token: string) {
+  try {
+    const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
+    const forwardedFor = (await headers()).get('x-forwarded-for')
+
+    const ip = forwardedFor ? forwardedFor.split(',')[0] : null
+    const result = await fetch(url, {
+      body: JSON.stringify({
+        secret: process.env.TURNSTILE_SECRET_KEY,
+        response: token,
+        remoteip: ip,
+      }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const outcome = await result.json()
+
+    return outcome
   } catch (error) {
     throw error
   }
