b21f68132bc991fb3db9fe104905fe73894ea922
Nguyễn Đức Thiện <nguyenducthien9@gmail.com>
2025-03-18 16:19:18 +0000

    feat: add image preview

--- src/app/components/image-with-link.tsx
+++ src/app/components/image-with-link.tsx
@@ -1,3 +1,6 @@
+import { Anchor, AspectRatio, Overlay } from '@mantine/core'
+import { useHover } from '@mantine/hooks'
+import { IconDownload } from '@tabler/icons-react'
 import Image from 'next/image'
 
 interface Props {
@@ -6,15 +9,25 @@ interface Props {
 }
 
 function ImageWithLink({ objectKey, presignedLink }: Props) {
+  const { hovered, ref } = useHover()
+
   return (
-    <Image
-      src={presignedLink}
-      alt={objectKey}
-      width={250}
-      height={250}
-      onClick={() => window.open(presignedLink, '_blank')}
-      className="cursor-pointer"
-    />
+    <AspectRatio ratio={16 / 9} maw={400} mx="auto" pos="relative" ref={ref}>
+      <Anchor href={presignedLink} target="_blank">
+        <Image
+          src={presignedLink}
+          alt={objectKey}
+          width={250}
+          height={250}
+          className="cursor-pointer"
+        />
+        {hovered && (
+          <Overlay color="gray" backgroundOpacity={0.5} center>
+            <IconDownload size={48} color="#fff" />
+          </Overlay>
+        )}
+      </Anchor>
+    </AspectRatio>
   )
 }
 
