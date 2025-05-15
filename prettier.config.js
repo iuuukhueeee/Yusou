a9e3e1fc6bc57322ec3b7557d5af28b49518c183
iuuukhueeee <nguyenducthien9@gmail.com>
2025-05-15 21:23:31 +0700

    feat: add prettier config rules

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  backetSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
+}
+
+module.exports = config
