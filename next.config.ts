4115376ab0cc949595af74c78a03849b252c1f58
iuuukhueeee <nguyenducthien9@gmail.com>
2025-06-02 22:21:25 +0700

    feat: remove redirect config

--- next.config.ts
+++ next.config.ts
@@ -17,15 +17,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/upload',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
