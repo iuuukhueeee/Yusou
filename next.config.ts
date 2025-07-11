324daba222dba07745d3c6076ac8de071d04a51f
iuuukhueeee <nguyenducthien9@gmail.com>
2025-07-11 09:24:51 +0700

    feat: remove redirects for better SEO

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/upload',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 }
 
 export default nextConfig
