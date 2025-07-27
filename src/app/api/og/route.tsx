a82892c8059faa87e4558f8cb847ce511327694e
iuuukhueeee <nguyenducthien9@gmail.com>
2025-07-27 10:45:14 +0700

    feat: add og

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,83 @@
+import fs from 'fs/promises'
+import { ImageResponse } from 'next/og'
+import path from 'path'
+// App router includes @vercel/og.
+// No need to install it.
+
+export const config = {
+  runtime: 'edge',
+}
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url)
+
+    // ?title=<title>
+    const hasTitle = searchParams.has('title')
+    const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'My default title'
+
+    const imagePath = path.join(process.cwd(), 'public', 'logo.png')
+    const imageData = await fs.readFile(imagePath)
+    const imageBase64 = `data:image/png;base64,${imageData.toString('base64')}`
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'black',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              justifyItems: 'center',
+            }}
+          >
+            <img
+              alt="Yusou"
+              height={200}
+              src={imageBase64}
+              style={{ margin: '0 30px' }}
+              width={232}
+            />
+          </div>
+          <div
+            style={{
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'white',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {title}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    )
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    console.log(`${e.message}`)
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
+}
